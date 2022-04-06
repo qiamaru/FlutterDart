7760fb37ff2662fe1e176a85d78f7c64f309cc32
qiamaru <amirulafiq.shahdan@gmail.com>
2022-04-06 17:41:42 +0800

    commit on 'FlutterDart'

---
+++ tuto1q1.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  var firstname = "Amirul";
+  dynamic lastname = "Afiq";
+  
+  int year = 2022;
+  int birthyear = 2000;
+  int age = year - birthyear;
+  
+  double cgpa = 3.5;
+  String address = "sentul, WPKL";
+   
+  print("My name is $firstname $lastname. I am $age this year. I am staying at $address. My CGPA is $cgpa");
+}
