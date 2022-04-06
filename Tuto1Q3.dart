6d0adcbb4a6e58711e99ca72595b969210b9fcf6
qiamaru <amirulafiq.shahdan@gmail.com>
2022-04-06 17:31:01 +0800

    commit on 'FlutterDart'

---
+++ Tuto1Q3.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main() {
+  show();
+}
+void show() {
+  print('The number is \$${getNumber()}\n');
+}
+int getNumber() {
+  stdout.write("Input: ");
+  int input = int.parse(stdin.readLineSync()!);
+  return input;
+}
+void anotherShow(int num) {
+  print("Show this number: $num");
+}
+dynamicValue() {
+  return ;
+}
